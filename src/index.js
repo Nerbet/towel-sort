0c87810479b308250d383f1a9f717f7e20f7cdca
Nerbet <60988912+Nerbet@users.noreply.github.com>
2020-03-22 20:45:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    var digits = [];
+    m = matrix.length;
+    n = matrix[0].length;
+    for(var i=0; i<m; i++)
+    {
+        if(i%2==0)
+        {
+            for(var j=0; j<n; j++)
+            {
+                digits.push(matrix[i][j]);
+            }
+        
+        }
+        else
+        {
+            for(var j=2; j>-1; j--)
+            {
+                digits.push(matrix[i][j]);
+            }
+        
+        }
+    }
+    return digits;
+  }
